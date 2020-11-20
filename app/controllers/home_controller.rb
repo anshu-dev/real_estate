5f4ccd284a5e6faa30010eb6d13aa8c61c572206
Anshu <anshu902kumar@gmail.com>
2020-11-20 12:21:50 +0530

    Buy button funtionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,21 @@
 class HomeController < ApplicationController
   def index
-  	@houses = House.latest_houses
-  	@complex_buildings = ComplexBuilding.latest_complexes
-  	@commercial_units = CommercialUnit.latest_commercials
+    @houses = House.latest_houses
+    @complex_buildings = ComplexBuilding.latest_complexes
+    @commercial_units = CommercialUnit.latest_commercials
   end
 
-	def very_long_task
-		byebug
-  	#sleep 60
-  	ManagerMailer.mail_info(current_manager.email).deliver
-		#@users = User.all
-	end
+  def very_long_task
+    sleep 60
+    if params[:table_name].eql?('commercial_unit')
+      recipient_email = CommercialUnit.find(params[:id]).manager.email
+    elsif params[:table_name].eql?('complex_buildings')
+      recipient_email = ComplexBuilding.find(params[:id]).manager.email
+    elsif params[:table_name].eql?('houses')
+      recipient_email = House.find(params[:id]).manager.email
+    end
+
+    ManagerMailer.buyer(current_manager, recipient_email).deliver
+    ManagerMailer.seller(current_manager, recipient_email).deliver
+  end
 end
--- app/mailers/manager_mailer.rb
+++ app/mailers/manager_mailer.rb
@@ -1,6 +1,13 @@
 class ManagerMailer < ApplicationMailer
-	def mail_info(user)
+  def buyer(user, recipient_email)
     @user = user
-    mail(to: @user.email, subject: 'Test Email for Letter Opener')
+    @recipient_email = recipient_email
+    mail(from:@user.email,  to: @recipient_email, subject: 'Want to buy property')
+  end
+
+  def seller(user, recipient_email)
+    @user = user
+    @recipient_email = recipient_email
+    mail(from:@recipient_email,  to:@user.email, subject: 'Okay, I send you details')
   end
 end
